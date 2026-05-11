160eb949662fb29c98e120875d77982ea1d37a76
tomasote572 <tomas.ramirez@mail.escuelaing.edu.co>
2026-05-10 19:40:05 -0500

    cleanup: remove terraform infrastructure files

--- infra/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "public_ip_address" {
-  description = "Dirección IP pública del servidor CardioTwin. Accede a través de http://<IP> en tu navegador."
-  value       = azurerm_linux_virtual_machine.vm.public_ip_address
-}
